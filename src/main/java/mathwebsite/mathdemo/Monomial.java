b6e27d0df7d4d82400655eaf72d2ac45c4e6aaa6
marcbgold <marcbgold@yahoo.com>
2018-02-16 15:18:06 -0500

    added a few output methods

--- src/main/java/mathwebsite/mathdemo/Monomial.java
+++ src/main/java/mathwebsite/mathdemo/Monomial.java
@@ -8,6 +8,20 @@ public class Monomial {
 	private int coefficient;
 	private List<Term> terms;
 
+	// TODO see if any contructors are unnecessary
+
+	public Monomial(int coefficient, String variable, int exponent) {
+		this.coefficient = coefficient;
+		terms = new ArrayList<>();
+		terms.add(new Term(variable, exponent));
+	}
+
+	public Monomial(int coefficient, Term term) {
+		this.coefficient = coefficient;
+		terms = new ArrayList<>();
+		terms.add(term);
+	}
+
 	public Monomial(int coefficient, ArrayList<Term> terms) {
 		this.coefficient = coefficient;
 		this.terms = terms;
@@ -21,4 +35,13 @@ public class Monomial {
 		return terms;
 	}
 
+	@Override
+	public String toString() {
+		String termOutput = "";
+		for (Term i : terms) {
+			termOutput = termOutput + i.getVariable() + i.getExponent();
+		}
+
+		return coefficient + termOutput;
+	}
 }
